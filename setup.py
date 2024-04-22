afc48ccfb745a6c632462f5977a9a2c9123e8a1c
王佳函 <mr_jia_han@qq.com>
2024-04-22 09:59:00 +0800

    调整-安装依赖问题

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PySqlModel",
-    version="1.1.3",
+    version="1.1.4",
     author="HanFengFeng",
     author_email="mr_jia_han@qq.com",
     description="简单方便的数据库查询包",
@@ -16,15 +16,15 @@ setup(
     url="https://github.com/NeverStopDreamingWang/pysqlmodel",
     # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
     packages=find_packages(),
-    install_requires=[
-        "pymysql",
-        "sqlite3"
-    ],
+    python_requires='>=3',
+    install_requires=[],
+    license="BSD 3-Clause",
     project_urls={
         "github": "https://github.com/NeverStopDreamingWang/pysqlmodel",
         "gitee": "https://gitee.com/NeverStopDreamingWang/pysqlmodel",
     }
 )
 
+# python setup.py check
 # python setup.py sdist bdist_wheel
 # twine upload dist/*
